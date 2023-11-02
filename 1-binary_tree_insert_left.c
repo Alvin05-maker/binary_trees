1869b21415eca6091af92f1fd0cc2319c4d301c2
Alvin05-maker <alvinoagaromba22@gmail.com>
2023-11-02 10:17:09 -0700

    insert node to the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	NewNode = binary_tree_node(parent, value);
 	if (parent == NULL)
+	{
 		return (NULL);
+	}
 	if (NewNode == NULL)
 	{
 		return (NULL);
